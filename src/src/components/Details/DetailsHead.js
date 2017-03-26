7247a91588cde8c22360dda12cb0cc6a67381335
Steve Marsh <s.marsh821@gmail.com>
2017-03-26 19:43:45 -0400

    Updated date bug:

--- src/src/components/Details/DetailsHead.js
+++ src/src/components/Details/DetailsHead.js
@@ -28,19 +28,19 @@ class DetailsHead extends Component {
     }
     getDay(dayNumber) {
         switch(dayNumber) {
-            case 0: 
+            case 6: 
                 return "Sunday";
-            case 1:
+            case 0:
                 return "Monday";
-            case 2:
+            case 1:
                 return "Tuesday";
-            case 3: 
+            case 2: 
                 return "Wednesday";
-            case 4: 
+            case 3: 
                 return "Thusday";
-            case 5:
+            case 4:
                 return "Friday";
-            case 6:
+            case 5:
                 return "Saturday";
         }
     }
